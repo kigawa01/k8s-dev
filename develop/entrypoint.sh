7796758e75f66e13d5ec5a90f4407abada3cc131
kigawa <kigawa.8390@gmail.com>
2024-07-19 11:58:10 +0900

    update

--- develop/entrypoint.sh
+++ develop/entrypoint.sh
@@ -2,23 +2,9 @@
 
 set -ue
 
-echo "Changing to home directory..."
-cd
-
-
-echo "Checking if ./.ssh directory exists..."
-if [ ! -d ./.ssh ]; then
-    mkdir "./.ssh" || { echo "Error creating directory './.ssh'"; exit 1; }
-fi
-
-echo "Checking if PUBKEY variable is set..."
-if [ -z "${PUBKEY:-}" ]; then
-    echo "PUBKEY is unset or set to the empty string"
-    exit 1
-else
-    echo "Adding PUBKEY to authorized_keys..."
-    echo "${PUBKEY}" >> ./.ssh/authorized_keys || { echo "Error appending to authorized_keys"; exit 1; }
-fi
+for filename in /script/init/* ; do
+  bash "$filename"
+done
 
 echo "Starting sshd..."
 /usr/sbin/sshd -D -e
