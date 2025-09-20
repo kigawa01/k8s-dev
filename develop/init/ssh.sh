715751b792d9845602deb31ec9f3960c96a2aeca
kigawa <mail@kigawa.net>
2025-09-21 03:24:20 +0900

    Comment out SSH host key initialization in `ssh.sh`.

--- develop/init/ssh.sh
+++ develop/init/ssh.sh
@@ -2,9 +2,9 @@
 
 set -ue
 
-
-echo "$HOST_ECDSA_KEY" > /etc/ssh/ssh_host_ecdsa_key
-echo "$HOST_ED25519_KEY" > /etc/ssh/ssh_host_ed25519_key
-echo "$HOST_RSA_KEY" > /etc/ssh/ssh_host_rsa_key
-
-chmod 600 /etc/ssh/*key
+#
+#echo "$HOST_ECDSA_KEY" > /etc/ssh/ssh_host_ecdsa_key
+#echo "$HOST_ED25519_KEY" > /etc/ssh/ssh_host_ed25519_key
+#echo "$HOST_RSA_KEY" > /etc/ssh/ssh_host_rsa_key
+#
+#chmod 600 /etc/ssh/*key
