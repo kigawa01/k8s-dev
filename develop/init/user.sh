989fb3a6d210156866ddc8925b351feba690e4c7
kigawa <mail@kigawa.net>
2026-02-17 14:44:41 +0900

    Comment out SSH host key initialization in `ssh.sh`.

--- develop/init/user.sh
+++ develop/init/user.sh
@@ -3,8 +3,9 @@
 adduser --disabled-password --gecos "" "$USER"
 echo "${USER}:${PASS}" | chpasswd
 gpasswd -a "${USER}" sudo
-chown "${USER}:${USER}" /home/${USER}/
-chown "${USER}:${USER}" /home/${USER}/*
+chown "${USER}:${USER}" /home/${USER}
 chown -R "${USER}:${USER}" /home/${USER}/.ssh
+
+chmod 700 /home/${USER}
 
 bash -c "$CMD_USER_CREATED"
